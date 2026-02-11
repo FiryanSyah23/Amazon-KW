bb9d7cf718897bf9e76a805bd4734bf6b3653a78
FiryanSyah <firyansyah22@gmail.com>
2026-02-11 21:09:32 +0700

    simpan isi dari cart ke local storage browser

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,24 @@
-export let cart = [
-  {
-    productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productID: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    quantity: 1,
-  },
-];
+//------------------------------------------
+//  Tempat Simpan Object
+//------------------------------------------
+
+export let cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart) {
+  cart = [
+    {
+      productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productID: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 //------------------------------------------
 // fungsi menabahkan objek pada cart array
@@ -16,7 +27,7 @@ export const addtocart = (product_id) => {
   // cari nilai id yang sama
   let matchingItem;
   cart.forEach((cartItem) => {
-    if (product_id === cartItem.productId) {
+    if (product_id === cartItem.productID) {
       matchingItem = cartItem;
     }
   });
@@ -25,21 +36,24 @@ export const addtocart = (product_id) => {
     matchingItem.quantity += 1;
   } else {
     matchingItem = {
-      productId: product_id,
+      productID: product_id,
       quantity: 1,
     };
     cart.push(matchingItem);
   }
+  saveToStorage();
 };
 
+//------------------------------------------
+// fungsi Menghapus objek pada cart array
+//------------------------------------------
 export const removeFromCart = (cardIDProduct) => {
   const newCart = [];
-
   cart.forEach((cartItem) => {
     if (cartItem.productID !== cardIDProduct) {
       newCart.push(cartItem);
     }
   });
-
   cart = newCart;
+  saveToStorage();
 };
